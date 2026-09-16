d6cbee13d06f97b29e876ecdbd561e4d9c201a14
mukundgithubhot <mukund_hot@hotmail.com>
2026-09-16 11:16:07 +0530

    Build 3

--- Mouse.gd
+++ Mouse.gd
@@ -7,17 +7,29 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	global_position = get_global_mouse_position()
+	
 	if has_overlapping_bodies():
 		var bs = get_overlapping_bodies()
 		for b in bs:
 			if not (b in moving):
 				moving.append(b)
-				b.print_tree()
+	
 	if not Input.is_action_pressed("move"):
+		for b in moving:
+			b.get_child(0).visible = false
+			
 		moving.clear()
+	
+	if Input.is_action_just_pressed("Dele"):
+		for b in moving:
+			b.queue_free()
+		moving.clear()
+	
 	for b in moving:
-		b.apply_force(global_position - b.global_position)
+		b.apply_force(global_position - b.global_position - b.linear_velocity)
+		b.get_child(0).visible = true
 		
+	
 	pass
